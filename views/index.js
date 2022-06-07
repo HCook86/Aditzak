57a7c6823cdd02ad7d677060bbfce548f877e82c
HCook86 <93165051+HCook86@users.noreply.github.com>
2022-06-07 21:56:46 +0200

    Update index.js

--- views/index.js
+++ views/index.js
@@ -1,1 +1,14 @@
+$(function(){
 
+    eel.expose(say_hello_js);               // Expose this function to Python
+    function say_hello_js(x) {
+    console.log("Hello from " + x);
+    }
+    say_hello_js("Javascript World!");
+    eel.handleinput("connected!");  // Call a Python function
+    
+    $("#btn").click(function(){
+        eel.handleinput($("#inp").val());
+        $('#inp').val('');
+    });
+}); 
