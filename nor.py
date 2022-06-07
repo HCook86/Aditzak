3e2c3834bd54493e0e8b2c8df8590e083f034dd5
HCook86 <93165051+HCook86@users.noreply.github.com>
2022-06-07 18:47:17 +0200

    Update nor.py

--- nor.py
+++ nor.py
@@ -1,1 +1,19 @@
+from json import loads
 
+ADITZA = input("Aditza: ")
+
+def nor(aditza):
+    hand = open("nor.json", "r")
+    info = hand.read()
+    NOR = loads(info)
+
+    erantzuna = None
+    for element in NOR:
+        tipo = NOR[element]
+        for denbora in tipo:
+            verbos = tipo[denbora]
+            for verb in verbos.items():
+                if aditza == verb[1]:
+                    erantzuna = element
+    return erantzuna
+print(nor(ADITZA))
