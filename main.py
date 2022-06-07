711e38ad1cd26bb666a139130636000edc934a90
Asempere123123 <44426824+Asempere123123@users.noreply.github.com>
2022-06-07 20:51:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,17 @@ class verb:
     ondorioa = False
     indikatiboa = False
 
-aditza = str("dizkioguke")
+    nk = False
+    ni = False
+    nik = False
+    lehen = False
+
+aditza = str("diezazkigukegu")
+
+def remove_prefix(text, prefix):
+    if text.startswith(prefix):
+        return text[len(prefix):]
+    return text
 
 def nnk(aditz):
     return None
@@ -56,6 +66,101 @@ def ahaleraORondorioa(aditz):
             else:
                 aditz.ondorioa = True
 
+def analisisCaso(aditz):
+
+    v = aditz.verb
+    if aditz.indikatiboa == True:
+
+        v = remove_prefix(v, "nat")
+        v = remove_prefix(v, "gat")
+        v = remove_prefix(v, "zat")
+        v = remove_prefix(v, "nint")
+        v = remove_prefix(v, "zit")
+        v = remove_prefix(v, "gint")
+        v = remove_prefix(v, "zint")
+
+        if v[:3] == "zai":
+            aditz.ni = True
+        else:
+            v = aditz.verb
+
+            v = remove_prefix(v, "na")
+            v = remove_prefix(v, "d")
+            v = remove_prefix(v, "gait")
+            v = remove_prefix(v, "zait")
+            v = remove_prefix(v, "dit")
+            v = remove_prefix(v, "nind")
+            v = remove_prefix(v, "gint")
+            v = remove_prefix(v, "zint")
+
+            if v[:1] == "u":
+                aditz.nk = True
+            else:
+                aditz.nik = True
+
+    if aditz.baldintza == True:
+
+        v = remove_prefix(v, "ba")
+
+        v = remove_prefix(v, "nint")
+        v = remove_prefix(v, "lit")
+        v = remove_prefix(v, "gint")
+        v = remove_prefix(v, "zint")
+
+        if v[:3] == "zai":
+            aditz.ni = True
+        else: 
+            v = aditz.verb
+
+            v = remove_prefix(v, "ba")
+
+            v = remove_prefix(v, "nind")
+            v = remove_prefix(v, "gint")
+            v = remove_prefix(v, "zint")
+
+            if v[:1] == "u":
+                aditz.nk = True
+            else:
+                aditz.nik = True
+
+    if aditz.ondorioa == True:
+
+        v = remove_prefix(v, "nint")
+        v = remove_prefix(v, "lit")
+        v = remove_prefix(v, "gint")
+        v = remove_prefix(v, "zint")
+
+        if v[:3] == "zai":
+            aditz.ni = True
+        else: 
+            v = aditz.verb
+
+            v = remove_prefix(v, "nind")
+            v = remove_prefix(v, "gint")
+            v = remove_prefix(v, "zint")
+
+            if v[:1] == "u":
+                aditz.nk = True
+            else:
+                aditz.nik = True
+
+    if aditz.ahalera == True:
+        
+        v = remove_prefix(v, "na")
+        v = remove_prefix(v, "da")
+        v = remove_prefix(v, "ga")
+        v = remove_prefix(v, "za")
+
+        if v[:2] == "ki":
+            aditz.ni = True
+        else:
+            v = aditz.verb
+
+            if v[:5] == "dieza":
+                aditz.nik = True
+            else:
+                aditz.nk = True
+
 def main(verb):
     if nnk(verb) != None:
         return nnk(verb)
@@ -67,6 +172,8 @@ def main(verb):
 
     ahaleraORondorioa(verb)
 
-    return verb.ahalera
+    analisisCaso(verb)
+
+    return (verb.ahalera, verb.ondorioa, verb.indikatiboa, verb.baldintza)
 
 print(main(verb(aditza)))
