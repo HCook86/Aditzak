4c89624d94294c9bbe228bf2dc84c176f630c9d8
HCook86 <93165051+HCook86@users.noreply.github.com>
2022-06-08 23:17:43 +0200

    Update output.py

--- output.py
+++ output.py
@@ -1,1 +1,11 @@
+import eel
+from app import analyse
 
+eel.init('views')
+
+@eel.expose                         # Expose this function to Javascript
+def handleinput(x):
+    print(analyse(x))
+    #eel.display()
+
+eel.start('main.html')
