dd40c4206a79e0add60075fdcab52223c4fe6cab
Asempere123123 <44426824+Asempere123123@users.noreply.github.com>
2022-06-08 11:17:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from json import loads
+
 class verb:
     def __init__(self, verb):
         self.verb = verb
@@ -14,14 +16,35 @@ class verb:
     nik = False
     lehen = False
 
-aditza = str("diezazkigukegu")
+    nor = None
+    nori = None
+    nork = None
+
+aditza = str("gintudan")
 
 def remove_prefix(text, prefix):
     if text.startswith(prefix):
         return text[len(prefix):]
     return text
 
-def nnk(aditz):
+def nor(aditz):
+    
+    hand = open("nor.json", "r")
+    info = hand.read()
+    NOR = loads(info)
+
+    erantzuna = None
+    for element in NOR:
+        tipo = NOR[element]
+        for denbora in tipo:
+            verbos = tipo[denbora]
+            for verb in verbos.items():
+                if aditz.verb == verb[1]:
+                    erantzuna = {"Aditza":aditz.verb,"Kasua":"NOR", "Modua":element, "Denbora":denbora, "Nor":verb[0], "Nori":None, "Nork":None}
+                    break
+    return erantzuna
+
+def nornork(aditz):
     return None
 
 def baldintza(aditz):
@@ -71,30 +94,30 @@ def analisisCaso(aditz):
     v = aditz.verb
     if aditz.indikatiboa == True:
 
-        v = remove_prefix(v, "nat")
-        v = remove_prefix(v, "gat")
-        v = remove_prefix(v, "zat")
-        v = remove_prefix(v, "nint")
-        v = remove_prefix(v, "zit")
+        v = remove_prefix(v, "na")
+        v = remove_prefix(v, "d")
+        v = remove_prefix(v, "gait")
+        v = remove_prefix(v, "zait")
+        v = remove_prefix(v, "dit")
+        v = remove_prefix(v, "nind")
         v = remove_prefix(v, "gint")
         v = remove_prefix(v, "zint")
 
-        if v[:3] == "zai":
-            aditz.ni = True
+        if v[:1] == "u":
+            aditz.nk = True
         else:
             v = aditz.verb
 
-            v = remove_prefix(v, "na")
-            v = remove_prefix(v, "d")
-            v = remove_prefix(v, "gait")
-            v = remove_prefix(v, "zait")
-            v = remove_prefix(v, "dit")
-            v = remove_prefix(v, "nind")
+            v = remove_prefix(v, "nat")
+            v = remove_prefix(v, "gat")
+            v = remove_prefix(v, "zat")
+            v = remove_prefix(v, "nint")
+            v = remove_prefix(v, "zit")
             v = remove_prefix(v, "gint")
             v = remove_prefix(v, "zint")
 
-            if v[:1] == "u":
-                aditz.nk = True
+            if v[:3] == "zai":
+                aditz.ni = True
             else:
                 aditz.nik = True
 
@@ -161,9 +184,110 @@ def analisisCaso(aditz):
             else:
                 aditz.nk = True
 
+def indikativoPersonas(aditz):
+    if aditz.indikatiboa:
+        
+        v = aditz.verb
+        if aditz.ni:
+
+            if v[-1] == "n":
+                aditz.lehen = True
+
+            if v.startswith("na") or  v.startswith("nin"):
+                aditz.nor =  "ni"
+            if v.startswith("ga") or  v.startswith("gin"):
+                aditz.nor =  "gu"
+            
+            aditz.teend = False
+
+            if v.endswith("te"):
+                aditz.teend = True
+            else:
+                v = v.removesuffix("dan")
+                v = v.removesuffix("on")
+                v = v.removesuffix("gun")
+                v = v.removesuffix("zun")
+                v = v.removesuffix("zuen")
+                v = v.removesuffix("en")
+
+                if v.endswith("te"):
+                    aditz.teend = True
+            
+            if (v.startswith("za") and not aditz.teend) or  (v.startswith("zin") and not aditz.teend):
+                aditz.nor =  "zu"
+
+            if (v.startswith("za") and aditz.teend) or  (v.startswith("zin") and aditz.teend):
+                aditz.nor =  "zuek"
+
+            if v.startswith("zaizki") or v.startswith("zitzaizki"):
+                aditz.nor = "haiek"
+
+            elif v.startswith("zai") or  v.startswith("zit"):
+                aditz.nor =  "hura"
+
+            v = aditz.verb
+            v = v.removesuffix("te")
+            v = v.removesuffix("n")
+
+            if v.endswith("t") or  v.endswith("da"):
+                aditz.nori =  "niri"
+            if v.endswith("o"):
+                aditz.nori =  "hari"
+            if v.endswith("gu"):
+                aditz.nori =  "guri"
+            if v.endswith("zu"):
+                aditz.nori =  "zuri"
+            if v.endswith("zue"):
+                aditz.nori =  "zuei"
+            if v.endswith("e"):
+                aditz.nori =  "haiei"
+
+            print(aditz.nor, aditz.nori, aditz.lehen)
+
+        if aditz.nk:
+            
+            if v.endswith("n"):
+                aditz.lehen = True
+
+            if v.startswith("nau") or v.startswith("nindu"):
+                aditz.nor = "ni"
+            if v.startswith("du"):
+                aditz.nor = "hura"
+            if v.startswith("gaitu") or v.startswith("gintu"):
+                aditz.nor = "gu"
+            if v.startswith("zaituzte") or v.startswith("zintuzte"):
+                aditz.nor = "zuek"
+            elif v.startswith("zaitu") or v.startswith("zintu"): 
+                aditz.nor = "zu"
+            if v.startswith("ditu"):
+                aditz.nor = "haiek"
+            
+            if v.endswith("t") or v.endswith("dan"):
+                aditz.nork = "nik"
+            if v.endswith("u") or v.endswith("en"):
+                aditz.nork = "hark"
+            if v.endswith("gu") or v.endswith("gun"):
+                aditz.nork = "guk"
+            if v.endswith("zu") or v.endswith("zun"):
+                aditz.nork = "zuk"
+            if v.endswith("zue") or v.endswith("zuen"):
+                aditz.nork = "zuek"
+            if v.endswith("te") or v.endswith("ten"):
+                aditz.nork = "haiek"
+
+            print(aditz.nor, aditz.nork, aditz.lehen)
+
+        if aditz.nik:
+            
+            pass
+
 def main(verb):
-    if nnk(verb) != None:
-        return nnk(verb)
+
+    if nor(verb) != None:
+        return nor(verb)
+
+    if nornork(verb) != None:
+        return nornork(verb)
     
     baldintza(verb)
 
@@ -174,6 +298,8 @@ def main(verb):
 
     analisisCaso(verb)
 
-    return (verb.ahalera, verb.ondorioa, verb.indikatiboa, verb.baldintza)
+    indikativoPersonas(verb)
+
+    return (verb.ahalera, verb.ondorioa, verb.indikatiboa, verb.baldintza, "caso:", verb.ni, verb.nk, verb.nik)
 
 print(main(verb(aditza)))
