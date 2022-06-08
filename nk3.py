9833212ad1d6081bcfc9e82a1b24d6d7c2b70d60
HCook86 <93165051+HCook86@users.noreply.github.com>
2022-06-08 14:21:50 +0200

    Update nk3.py

--- nk3.py
+++ nk3.py
@@ -1,1 +1,16 @@
+from json import loads
 
+def nk3(aditza):
+    hand = open("nk3.json", "r")
+    info = hand.read()
+    NK3 = loads(info)
+    
+    erantzuna = False
+    for element in NK3:
+        tipo = NK3[element]
+        for zenbaki in tipo:
+            verbos = tipo[zenbaki]
+            for verb in verbos.items():
+                if aditza == verb[1]:
+                    erantzuna = {"Aditza":aditza,"Kasua":"NOR-NORK", "Modua":element, "Denbora":denbora, "Nor":"", "Nori":None, "Nork":verb[0]}
+    return erantzuna
