fd403a6e21001ef936f8a1f3ae494c2fcdeb8950
Asempere123123 <asempere@colegioaleman.net>
2022-06-09 17:12:46 +0200

    added subjuntive nor-nori analisis

--- extra.py
+++ extra.py
@@ -1,4 +1,7 @@
-from app import remove_prefix
+def remove_prefix(text, prefix):
+    if text.startswith(prefix):
+        return text[len(prefix):]
+    return text
 
 def subjuntivoa(aditz):
     
@@ -25,6 +28,80 @@ def subjuntivoa(aditz):
     
     if ki and nend:
         v = aditz.verb
-        v.removesuffix("n")
 
-        print("esubjuntivo")
+        v = remove_prefix(v, "na")
+        v = remove_prefix(v, "da")
+        v = remove_prefix(v, "ga")
+        v = remove_prefix(v, "za")
+
+        if v.startswith("ki"):
+            aditz.ni = True
+        elif v.startswith("dieza"):
+            aditz.nik = True
+        else:
+            aditz.nk = True
+
+        v = aditz.verb
+        v = v.removesuffix("n")
+
+        if aditz.ni:
+            
+            aditz.teend = False
+
+            if v.endswith("te"):
+                aditz.teend = True
+                v = v.removesuffix("te")
+
+            if v.startswith("naki"):
+                aditz.nor = "ni"
+            if v.startswith("daki") and not v.startswith("dakizki"):
+                aditz.nor = "hura"
+            if v.startswith("gakizki"):
+                aditz.nor = "gu"
+            if v.startswith("zakizki") and aditz.teend:
+                aditz.nor = "zuek"
+            elif v.startswith("zakizki"):
+                aditz.nor = "zu"
+            if v.startswith("dakizki"):
+                aditz.nor = "haiek"
+
+            if v.endswith("da"):
+                aditz.nori = "niri"
+            if v.endswith("o"):
+                aditz.nori = "hari"
+            if v.endswith("gu"):
+                aditz.nori = "guri"
+            if v.endswith("zu"):
+                aditz.nori = "zuri"
+            if v.endswith("zue"):
+                aditz.nori = "zuei"
+            if v.endswith("e") and not v.endswith("zue"):
+                aditz.nori = "haiei"
+
+        if aditz.nk:
+            pass
+
+        if aditz.nik:
+            pass
+
+
+        kasua = None
+        if aditz.nk:
+            kasua = "NOR-NORK"
+        elif aditz.ni:
+             kasua = "NOR-NORI"
+        elif aditz.nik:
+            kasua = "NOR-NORI-NORK"
+
+        modua = "Subjuntiboa"
+
+        denbora = None
+        if aditz.lehen == False:
+            denbora = "Oraina"
+        else:
+            denbora = "Iragana"
+
+        return {"Aditza":aditz.verb,"Kasua":kasua, "Modua":modua, "Denbora":denbora, "Nor":aditz.nor, "Nori":aditz.nori, "Nork":aditz.nork}
+                
+    else:
+        return None
