225a3c2db9ea3653ddf06f293d827be85c638772
Asempere123123 <asempere@colegioaleman.net>
2022-06-09 16:26:59 +0200

    subjuntive detection added

--- extra.py
+++ extra.py
@@ -1,9 +1,30 @@
+from app import remove_prefix
+
 def subjuntivoa(aditz):
     
     nend = False
+    ki = False
 
     v = aditz.verb
 
     if v.endswith("n"):
         nend = True
         v.removesuffix("n")
+
+    v = remove_prefix(v, "na")
+    v = remove_prefix(v, "da")
+    v = remove_prefix(v, "ga")
+    v = remove_prefix(v, "za")
+    v = remove_prefix(v, "de")
+    v = remove_prefix(v, "gait")
+    v = remove_prefix(v, "zait")
+    v = remove_prefix(v, "dit")
+
+    if v.startswith("ki") or v.startswith("za") or v.startswith("dieza"):
+        ki = True
+    
+    if ki and nend:
+        v = aditz.verb
+        v.removesuffix("n")
+
+        print("esubjuntivo")
