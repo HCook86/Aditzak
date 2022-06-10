690a8e081e7d1358bc57db56a07480781124d734
Asempere123123 <44426824+Asempere123123@users.noreply.github.com>
2022-06-10 12:09:02 +0200

    Added incorrect verb partial detektion

--- main.py
+++ main.py
@@ -10,7 +10,32 @@ def flash(error):
 @eel.expose                       
 def handleinput(x):
     if x != "" and not " " in x:
-        eel.out(dumps(analyse(x)))
+        x = analyse(x)
+
+        if x["Kasua"] == "NOR":
+            if x ["Nor"] != None and x ["Nori"] == None and x ["Nork"] == None:
+                eel.out(dumps(x))
+            else:
+                flash("Aditz hau ez da existitzen.")
+
+        if x["Kasua"] == "NOR-NORI":
+            if x ["Nor"] != None and x ["Nori"] != None and x ["Nork"] == None:
+                eel.out(dumps(x))
+            else:
+                flash("Aditz hau ez da existitzen.")
+
+        if x["Kasua"] == "NOR-NORK":
+            if x ["Nor"] != None and x ["Nori"] == None and x ["Nork"] != None:
+                eel.out(dumps(x))
+            else:
+                flash("Aditz hau ez da existitzen.")
+
+        if x["Kasua"] == "NOR-NORI-NORK":
+            if x ["Nor"] != None and x ["Nori"] != None and x ["Nork"] != None:
+                eel.out(dumps(x))
+            else:
+                flash("Aditz hau ez da existitzen.")
+
     else:
         flash("Aditz laguntzaile batekin sahiatu.")
 
